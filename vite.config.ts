1befed72afa09147f7825513f9d47fef2a998354
IKA3RUS <w@abhishekrein.xyz>
2026-09-16 11:45:28 +0530

    feat: add cloudflare workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
+import path from "path";
+
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
+import { cloudflare } from "@cloudflare/vite-plugin";
 import contentCollections from "@content-collections/vite";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+    tsconfigPaths: true,
+  },
   plugins: [
     devtools(),
     tailwindcss(),
@@ -17,6 +25,11 @@ const config = defineConfig({
     tanstackStart(),
     viteReact(),
     contentCollections(),
+    cloudflare({
+      viteEnvironment: {
+        name: "ssr",
+      },
+    }),
   ],
 });
 
